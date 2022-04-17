79f9c8118cc393dff27ed49feb7f9ec18a2f8064
pw <pangnima@gmail.com>
2022-04-17 19:39:12 +0900

    tete

---
+++ src/pages/PlacePage/PlaceList.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="container">
+		<div class="place-detail">
+			<div class="place-header">
+				<div class="thumb"></div>
+				<h2>BJ 킴성태의 플레이스</h2>
+				<button type="button"><span>즐겨찾기</span></button>
+				<span>인기글</span>
+				<div class="search">
+					<input type="text">
+				</div>
+			</div>
+			<div class="place-body">
+				<ul class="cate-list">
+					<li>#전체</li>
+					<li>#공지사항</li>
+					<li>#성태에게</li>
+					<li>#잡답</li>
+					<li>#일상</li>
+				</ul>
+
+				<div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name:'placePage',
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
